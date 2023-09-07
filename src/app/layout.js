5351f869f82faef64f4fd9fe654b59ffea16ea46
Elian Rehbani <elianrhbn@gmail.com>
2023-09-07 15:18:17 -0300

    open graph

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import Navbar from "@/components/navbar/page";
 import "./globals.css";
 import { Prompt } from "next/font/google";
 import Providers from "./providers";
+import Head from "next/head";
 
 const prompt = Prompt({
   weight: ["300", "500", "700"],
@@ -13,16 +14,18 @@ export const metadata = {
   title: "Elian's portfolio",
   description: "Elian's portfolio created with NextJS",
   keywords: "Portfolio, Elian, Full Stack developer, Front end developer, Back end developer, Henry"
-  
+  // AGUANTE EL TOTK
 };
 
 
 export default function RootLayout({ children }) {
   return (
     <html id="home" lang="en" className="!scroll-smooth" data-theme="dark">
-      <head>
-        {/* <link rel="icon" href="/faviconExtJS.png" sizes="any" /> */}
-      </head>
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <meta property="og:image" content="/open_graph.png"/>
+        <meta property="twitter:image" content="/open_graph.png"/>
+      </Head>
       <body className={prompt.className}>
        <Providers>
         <div className="background"></div>
