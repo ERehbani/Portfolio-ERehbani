8e58e9881874d1f03e98adcd93892f8ffd3677ca
Elian Rehbani <elianrhbn@gmail.com>
2023-09-07 17:03:36 -0300

    open graph

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,12 +23,12 @@ export default function RootLayout({ children }) {
     <html id="home" lang="en" className="!scroll-smooth" data-theme="dark">
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <meta property="og:image" content="https://portfolio-e-rehbani-rnfrzxa0a-erehbani.vercel.app/open_graph.png"></meta>
+        <meta property="og:image" content="https://portfolio-e-rehbani.vercel.app//open_graph.png"></meta>
         <meta property="og:title" content="Elian Full Stack Dev"></meta>
-        <meta property="og:description" content="Elian's Portfolio"></meta>
-        <meta property="twitter:image" content="https://portfolio-e-rehbani-rnfrzxa0a-erehbani.vercel.app/open_graph.png"></meta>
+        <meta property="og:description" content="Elian Rehbani | Portfolio"></meta>
+        <meta property="twitter:image" content="https://portfolio-e-rehbani.vercel.app//open_graph.png"></meta>
         <meta property="twitter:title" content="Elian Full Stack Dev"></meta>
-        <meta property="twitter:description" content="Elian's Portfolio"></meta>
+        <meta property="twitter:description" content="Elian Rehbani | Portfolio"></meta>
       </head>
       <body className={prompt.className}>
        <Providers>
