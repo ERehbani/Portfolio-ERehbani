401ed8129f97815df46fed309faa9f2004241322
Elian Rehbani <elianrhbn@gmail.com>
2023-09-11 12:22:22 -0300

    og:image

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,7 @@ export const metadata = {
   title: "Elian Rehbani | Portfolio",
   description: "Elian's portfolio created with NextJS",
   keywords: "Portfolio, Elian, Full Stack developer, Front end developer, Back end developer, Henry"
-  // AGUANTE EL TOTK
+
 };
 
 
@@ -23,10 +23,10 @@ export default function RootLayout({ children }) {
     <html id="home" lang="en" className="!scroll-smooth" data-theme="dark">
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <meta property="og:image" content="https://portfolio-e-rehbani.vercel.app//open_graph.png"></meta>
+        <meta property="og:image" content="https://i.imgur.com/6iKBoMB.png"/>
         <meta property="og:title" content="Elian Rehbani | Full Stack Dev"></meta>
         <meta property="og:description" content="Elian Rehbani | Portfolio"></meta>
-        <meta property="twitter:image" content="https://portfolio-e-rehbani.vercel.app//open_graph.png"></meta>
+        <meta property="twitter:image" content="https://i.imgur.com/6iKBoMB.png"/>
         <meta property="twitter:title" content="Elian Full Stack Dev"></meta>
         <meta property="twitter:description" content="Elian Rehbani | Portfolio"></meta>
       </head>
